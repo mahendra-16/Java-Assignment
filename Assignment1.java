deb8df0564d71fe2ab6c284ac089aff712bd9f02
mahendra-16 <prajapatimahendra@ymail.com>
2016-11-01 20:18:39 +0530

    myapp

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// variable declaration and assignment value
+		// operators
+		// Type Casting
+		
+		int num1 = 10;
+		float num2 = 25.5f;
+		
+		int sum = (int) (num1 + (float)num2);
+		System.out.println("Sum : " + sum);
+		
+	}
+
+}
